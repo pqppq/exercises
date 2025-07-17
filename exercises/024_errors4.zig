091921caf8331d97ef7bd5d7c4ff4c755a2156ca
pqppq <yutatsu0119@gmail.com>
2025-07-18 00:50:15 +0900

    024: catch and branch by err

--- exercises/024_errors4.zig
+++ exercises/024_errors4.zig
@@ -59,7 +59,19 @@ fn fixTooSmall(n: u32) MyNumberError!u32 {
     // If we get a TooSmall error, we should return 10.
     // If we get any other error, we should return that error.
     // Otherwise, we return the u32 number.
-    return detectProblems(n) ???;
+    return detectProblems(n) catch |err| {
+        // OK
+        if (err == MyNumberError.TooSmall) {
+            return 10;
+        }
+        return err;
+        // NG! (type must be same for each branch)
+        // switch (err) {
+        //     MyNumberError.TooSmall => return 10,
+        //     else => return err
+        // }
+
+    };
 }
 
 fn detectProblems(n: u32) MyNumberError!u32 {
